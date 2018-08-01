46e5ccaa11ce731a947a38556e0c2e66e0b72251
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-08-01 21:53:23 +0300

    Added Selection type

---
+++ Source/Types/Selection.swift
@@ -0,0 +1,15 @@
+//
+//  Selection.swift
+//  Flexible
+//
+//  Created by Igor Matyushkin on 29.07.2018.
+//  Copyright © 2018 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public enum Selection {
+    case first(count: Int)
+    case last(count: Int)
+    case all
+}
